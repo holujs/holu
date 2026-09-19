d9777da3a78c22e3e87b7863ac11bff9dbd902cf
KostyaTretyak <ktretiak.in.ua@gmail.com>
2026-09-19 16:43:32 +0300

    fix(examples): fix usage of injectorPerMod in an extension

--- examples/00-standalone-application/src/app/simple-extension.ts
+++ examples/00-standalone-application/src/app/simple-extension.ts
@@ -1,18 +1,16 @@
-import { injectable, Extension, Logger } from '@holu/core';
+import { injectable, Extension, Logger, type Injector } from '@holu/core';
 import { GreetingService } from './greeting.service.js';
 
 @injectable()
 export class AppExtension implements Extension {
-  constructor(
-    private greetingService: GreetingService,
-    private logger: Logger,
-  ) {}
+  constructor(private logger: Logger) {}
 
   async stage1() {
     this.logger.log('info', 'AppExtension: initializing in stage1.');
   }
 
-  async stage3() {
-    this.greetingService.greet('World');
+  async stage2(injectorPerMod: Injector) {
+    const greetingService = injectorPerMod.get(GreetingService);
+    greetingService.greet('World');
   }
 }
